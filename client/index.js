8dd7f0d143ce75635709a7cdd2951000388574fe
duke from earth <stephen_duke@live.com>
2019-12-19 15:14:39 -0500

    Add functionality

--- client/index.js
+++ client/index.js
@@ -1,57 +1,105 @@
 const socket = io();
+const search_data = ["Volcano", "Hurricane", "Earthquake"];
+var latest_data = {};
 
-if (!Detector.webgl) {
-    Detector.addGetWebGLMessage();
-} else {
+map.on('load', function () {
+    map.loadImage("/client/assets/fire-icon.png", function (error, image) {
+        if (error) throw error;
+        map.addImage("Volcano", image);
+        // Add a layer showing the places.
+    });
+    map.loadImage("/client/assets/earthquake-icon.png", function (error, image) {
+        if (error) throw error;
+        map.addImage("Earthquake", image);
+        // Add a layer showing the places.
+    });
+    map.loadImage("/client/assets/hurricane-icon.png", function (error, image) {
+        if (error) throw error;
+        map.addImage("Hurricane", image);
+        // Add a layer showing the places.
+    });
 
-    var years = ['Volcanoes', 'Hurricanes', 'Earthquakes'];
-    var container = document.getElementById('container');
-    var globe = new DAT.Globe(container);
+    socket.on('data_packet', function (type, data) {
+        map.addLayer({
+            'id': type,
+            'type': 'symbol',
+            'source': {
+                'type': 'geojson',
+                'data': data,
+            },
+            'layout': {
+                'icon-image': type,
+                "icon-size": .03,
+                'icon-allow-overlap': true
+            },
 
-    console.log(globe);
-    var i, tweens = [];
+        });
+    });
 
-    var settime = function (globe, t) {
-        return function () {
-            new TWEEN.Tween(globe).to({ time: t / years.length }, 500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-            var y = document.getElementById('year' + years[t]);
-            if (y.getAttribute('class') === 'year active') {
-                return;
-            }
-            var yy = document.getElementsByClassName('year');
-            for (i = 0; i < yy.length; i++) {
-                yy[i].setAttribute('class', 'year');
-            }
-            y.setAttribute('class', 'year active');
-            y.addEventListener('mouseover', socket.emit('hover', years[t]));
-        };
-    };
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'Hurricane', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
 
-    for (var i = 0; i < years.length; i++) {
-        console.log(years[i]);
-        var y = document.getElementById('year' + years[i]);
-        y.addEventListener('mouseover', settime(globe, i), false);
-    }
-
-    var xhr;
-    TWEEN.start();
-
-    xhr = new XMLHttpRequest();
-    xhr.open('GET', 'client/data/population909500.json', true);
-    xhr.onreadystatechange = function (e) {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                var data = JSON.parse(xhr.responseText);
-                window.data = data;
-                for (i = 0; i < data.length; i++) {
-                    globe.addData(data[i][1], { format: 'magnitude', name: data[i][0], animated: true });
-                }
-                globe.createPoints();
-                settime(globe, 0)();
-                globe.animate();
-                document.body.style.backgroundImage = 'none'; // remove loading
-            }
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'Hurricane' || 'Earthquake', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'Hurricane', function () {
+        map.getCanvas().style.cursor = '';
+    });
+});
+
+var setActive = function (type) {
+    console.log(type);
+    return function () {
+        var y = document.getElementById(search_data[type]);
+        if (y.getAttribute('class') === 'year active') {
+            return;
+        }
+        var yy = document.getElementsByClassName('year');
+        for (i = 0; i < yy.length; i++) {
+            yy[i].setAttribute('class', 'year');
+        }
+        y.setAttribute('class', 'year active');
+        active = type;
+        // y.addEventListener('mouseover', socket.emit('hover', search_data[type]));
     };
-    xhr.send(null);
+};
+var active = "0";
+var send = document.getElementById("sendbutton");
+var years = document.getElementById("amount");
+console.log(send);
+send.addEventListener('click', function () {
+    let trig = false;
+    for (let id in map.getStyle().layers) {
+        layer = map.getStyle().layers[id];
+        if (layer.id === search_data[active]) {
+            map.removeLayer(layer.id);
+            map.removeSource(layer.id);
+            trig = true;
+        }
+    }
+    if (!trig) socket.emit('hover', search_data[active], years.value);
+});
+
+for (var i = 0; i < search_data.length; i++) {
+    var y = document.getElementById(search_data[i]);
+    y.addEventListener('mouseover', setActive(i));
 }
--- server.mjs
+++ server.mjs
@@ -4,7 +4,70 @@ import express from 'express';
 import http from 'http';
 import path from 'path';
 import socketIO from 'socket.io';
+import mysql from 'mysql';
 
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "globeclient",
+  password: "globeclient"
+});
+
+
+con.connect(function (err) {
+  if (err) throw err;
+  // con.query("SELECT Latitude as lat, Longitude as lon, Magnitude/(select max(Magnitude) from dbglobe.disaster_data ) as mag FROM dbglobe.disaster_data", function (err, result, fields) {
+  //   if (err) throw err;
+  //   db_data = result;
+  //   console.log(db_data);
+  // });
+  console.log("Connected!");
+});
+
+function queryAllData() {
+  var db_data = {};
+  let search_data = ["Volcano", "Hurricane", "Earthquake"];
+  for (let i = 0; i < search_data.length; i++) {
+    let q = "SELECT Date as date, Latitude as lat, Longitude as lon, Magnitude as mag from dbglobe.disaster_data d where d.EventID = ?";
+    con.query(q, [search_data[i], search_data[i]], function (err, result, fields) {
+      if (err) throw err;
+      else {
+        db_data[search_data[i]] = { data: result };
+      }
+    });
+  }
+  return db_data;
+}
+
+function mapify(data, d_low, d_high) {
+  let rv = [];
+  let d1 = new Date(d_low);
+  let d2 = new Date(d_high+"/12/31");
+  for (let i in data) {
+    let event = data[i];
+    let event_date = new Date(event.date);
+    if (d1.getTime() < event_date && d2.getTime() > event_date) {
+      rv.push({
+        'type': 'Feature',
+        'properties': {
+          'description': "Date: " + event.date + "\n Magnitude: " + event.mag,
+          'icon': 'volcano',
+          'iconSize': [100]
+        },
+        'geometry': {
+          'type': 'Point',
+          'coordinates': [event.lon, event.lat]
+        }
+      });
+    }
+  }
+  return {
+    'type': 'FeatureCollection',
+    'features': rv
+  };
+}
+
+const db_data = queryAllData();
 const __dirname = path.resolve(path.dirname(''));
 const HOST = process.env.HOST || '0.0.0.0';
 const environment = process.env.ENV || "prod";
@@ -37,8 +100,10 @@ io.on('connection', function (socket) {
     delete sockets[socket.id];
   });
 
-  socket.on('hover', function (data) {
-    console.log(data);
+  socket.on('hover', function (type, years) {
+    let start_year = years.substr(0, 4);
+    let end_year = years.substr(7, 10);
+    let rv = db_data[type];
+    if (rv) socket.emit('data_packet', type, mapify(db_data[type].data,start_year,end_year));
   });
-
 });
